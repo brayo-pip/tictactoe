fa0cbefd5f8819b076fe051c8b8cbbf3ce456e3c
brayo <vukubrian@gmail.com>
2023-12-08 13:04:32 +0300

    add tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,90 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:tictactoe/main.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('App starts with an empty board', (WidgetTester tester) async {
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => TicTacToeModel(),
+      child: const MyApp(),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the board starts empty.
+    expect(find.text('X'), findsNothing);
+    expect(find.text('O'), findsNothing);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+  testWidgets('Tapping a tile changes it to "X"', (WidgetTester tester) async {
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => TicTacToeModel(),
+      child: const MyApp(),
+    ));
+
+    // Tap the first tile.
+    await tester.tap(find.byType(GestureDetector).first);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the first tile is now "X".
+    expect(find.text('X'), findsOneWidget);
+  });
+
+  testWidgets('Tapping a tile twice doesn\'t change it to "O"',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => TicTacToeModel(),
+      child: const MyApp(),
+    ));
+
+    // Tap the first tile twice.
+    await tester.tap(find.byType(GestureDetector).first);
+    await tester.pump();
+    await tester.tap(find.byType(GestureDetector).first);
+    await tester.pump();
+
+    // Verify that the first tile is still "X".
+    expect(find.text('X'), findsOneWidget);
+    expect(find.text('O'), findsNothing);
+  });
+
+  testWidgets('Tapping a different tile after the first one changes it to "O"',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => TicTacToeModel(),
+      child: const MyApp(),
+    ));
+
+    // Tap the first tile, then the second one.
+    await tester.tap(find.byType(GestureDetector).at(0));
+    await tester.pump();
+    await tester.tap(find.byType(GestureDetector).at(1));
+    await tester.pump();
+
+    // Verify that the first tile is "X" and the second one is "O".
+    expect(find.text('X'), findsOneWidget);
+    expect(find.text('O'), findsOneWidget);
+  });
+
+  testWidgets('A win resets the board', (WidgetTester tester) async {
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => TicTacToeModel(),
+      child: const MyApp(),
+    ));
+
+    // Tap the first row for X.
+    await tester.tap(find.byType(GestureDetector).at(0));
+    await tester.pump();
+    await tester.tap(find.byType(GestureDetector).at(3));
+    await tester.pump();
+    await tester.tap(find.byType(GestureDetector).at(1));
+    await tester.pump();
+    await tester.tap(find.byType(GestureDetector).at(4));
+    await tester.pump();
+    await tester.tap(find.byType(GestureDetector).at(2));
+    await tester.pump();
+
+    // Verify that the board is empty.
+    expect(find.text('X'), findsNothing);
+    expect(find.text('O'), findsNothing);
   });
 }
